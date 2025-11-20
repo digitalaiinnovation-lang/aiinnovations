443001ded7b3d5e8f9037a8e4d203497c6ea7d66
1 <digital.ai.innovation@gmail.com>
2025-11-20 09:46:41 +0200

    updated so it can work on cloudflare

--- src/app/api/inquiry/route.js
+++ src/app/api/inquiry/route.js
@@ -1,8 +1,6 @@
-'use server';
-
 import resend from "../../../../resend.config";
 import { NextResponse } from "next/server";
-export const config = { runtime: 'edge' };
+export const runtime = 'edge';
 
 export async function POST(req) {
   try {
